effe163298eefe82240f0807a2860682a5508f74
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2024-01-15 18:49:11 +0200

    1

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
- 
+#include "PriorityQueue.h"
+
+int main() {
+    PriorityQueue<int> priorityQueue;
+
+    priorityQueue.InsertWithPriority(5, 2);
+    priorityQueue.InsertWithPriority(10, 1);
+    priorityQueue.InsertWithPriority(3, 3);
+    priorityQueue.Show(); 
+
+    int highestPriority = priorityQueue.PullHighestPriorityElement();
+    std::cout << "Pulled highest priority element: " << highestPriority << "\n"; 
+
+    int peekedElement = priorityQueue.Peek();
+    std::cout << "Peeked element: " << peekedElement << "\n"; 
+
+    priorityQueue.Show(); 
+
+    return 0;
+}
